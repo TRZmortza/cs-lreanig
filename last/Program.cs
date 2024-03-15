6ec793d48cf49f4a397f1b47577983bf4228a5f1
TRZmorteza <mortezanoei664@Gmail.com>
2024-03-15 18:46:47 +0330

    modified:   last/Program.cs

--- last/Program.cs
+++ last/Program.cs
@@ -3,12 +3,12 @@
 // Console.Write("name is :"+name+"\n");
 // Console.Write($"name is:{name}\n");//some things
 // Console.Write("name is :{0}age is:{1}",name,age);
-
+/*
 string name=string.Empty;int age=0;
 Console.Write("enter your name:");
 name=Console.ReadLine();//input out put
 Console.Write("enter your age:");
 age=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(name+age);
-
+*/
 
