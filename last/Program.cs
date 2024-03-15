8bf9f58d4830be834b7a32737600790788e4ff80
morteza <148478332+TRZmortza@users.noreply.github.com>
2024-03-15 17:27:28 +0330

    Add files via upload

---
+++ last/Program.cs
@@ -0,0 +1,14 @@
+﻿// String name="morteza ";
+// int age=22;
+// Console.Write("name is :"+name+"\n");
+// Console.Write($"name is:{name}\n");//some things
+// Console.Write("name is :{0}age is:{1}",name,age);
+
+string name=string.Empty;int age=0;
+Console.Write("enter your name:");
+name=Console.ReadLine();//input out put
+Console.Write("enter your age:");
+age=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(name+age);
+
+
