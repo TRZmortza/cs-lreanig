c6f718971688e0b19ff36389524b56a1c118f52b
TRZmorteza <mortezanoei664@Gmail.com>
2024-03-15 20:00:15 +0330

    modified:   last/Program.cs

--- last/Program.cs
+++ last/Program.cs
@@ -12,3 +12,22 @@ age=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(name+age);
 */
 
+//this prat i am going to write a progrem a simpel one for taking a age 
+// and then its going ti tell Retirement age in iran //(its going to tell how meny years left untill Retirement)
+/*
+int RetirementAge=0,totalYearsLeft=0;
+Console.WriteLine("enter your age: ");
+int userAge=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter your name: ");
+RetirementAge=60;
+string userName=Console.ReadLine();
+totalYearsLeft=RetirementAge-userAge;
+if (totalYearsLeft>0)
+{
+    Console.WriteLine("{0} years of safring left {1}",totalYearsLeft,userName);
+}
+else{
+    Console.WriteLine("you been retired this many year {0} mr/ms:{1}",(totalYearsLeft*-1),userName);
+
+}
+*/
