c6780e0c04f341f9be69c8800caa4131c7b99c0f
TRZmorteza <mortezanoei664@gmail.com>
2024-03-30 14:30:15 -0700

    new test

--- last/Program.cs
+++ last/Program.cs
@@ -3,7 +3,7 @@
 // Console.Write("name is :"+name+"\n");
 // Console.Write($"name is:{name}\n");//some things
 // Console.Write("name is :{0}age is:{1}",name,age);
-/*
+/*//new test
 string name=string.Empty;int age=0;
 Console.Write("enter your name:");
 name=Console.ReadLine();//input out put
